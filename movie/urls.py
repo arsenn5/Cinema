4b92312e0593d6344bb998e5ac27d9df1148bf4c
Eldiyar1 <mc.oks1ne@gmail.com>
2023-12-14 16:14:51 +0600

    5

--- movie/urls.py
+++ movie/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
-from .views import movie_list_view, movie_detail_view, create_movie_view, delete_movie_view, update_movie_view
+from .views import MovieListView, MovieDetailView, CreateMovieView, DeleteMovieView, UpdateMovieView, Search
+
+# from .views import movie_list_view, movie_detail_view, create_movie_view, delete_movie_view, update_movie_view
+#
+# urlpatterns = [
+#     path('movies/', movie_list_view),
+#     path('movies/<int:id>/', movie_detail_view),
+#     path('movies/create/', create_movie_view),
+#     path('movies/<int:id>/update/', update_movie_view),
+#     path('movies/<int:id>/delete/', delete_movie_view),
+# ]
 
 urlpatterns = [
-    path('movies/', movie_list_view),
-    path('movies/<int:id>/', movie_detail_view),
-    path('movies/create/', create_movie_view),
-    path('movies/<int:id>/update/', update_movie_view),
-    path('movies/<int:id>/delete/', delete_movie_view),
-    # path('movies/<int:id>/review/', movie_review_view)
+    path('movies/', MovieListView.as_view()),
+    path('movies/<int:id>/', MovieDetailView.as_view()),
+    path('movies/create/', CreateMovieView.as_view()),
+    path('movies/<int:id>/update/', UpdateMovieView.as_view()),
+    path('movies/<int:id>/delete/', DeleteMovieView.as_view()),
+    path('search/', Search.as_view(), name='search'),
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,47 +1,110 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 
 from movie.forms import MovieForm
 from movie.models import Movie, Review
 
 
-def movie_list_view(request):
-    movies = Movie.objects.all()
-    return render(request, 'movies/movie_list.html', {'movies': movies})
+class MovieListView(ListView):
+    queryset = Movie
+    template_name = 'movies/movie_list.html'
+
+    def get_queryset(self):
+        return Movie.objects.all()
 
 
-def movie_detail_view(request, id):
-    movie = get_object_or_404(Movie, id=id)
-    return render(request, 'movies/movie_detail.html', {'movie': movie})
+# def movie_list_view(request):
+#     movies = Movie.objects.all()
+#     return render(request, 'movies/movie_list.html', {'movies': movies})
 
 
-def create_movie_view(request):
-    if request.method == 'POST':
-        form = MovieForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Успешно добавлен <a href="/"> На главную </a> ')
-    else:
-        form = MovieForm()
-    return render(request, 'movies/movie_create.html', {'form': form})
+class MovieDetailView(DetailView):
+    template_name = 'movies/movie_detail.html'
+
+    def get_object(self, **kwargs):
+        movie_id = self.kwargs.get('id')
+        return get_object_or_404(Movie, id=movie_id)
 
 
-def update_movie_view(request, id):
-    movie = get_object_or_404(Movie, id=id)
-    if request.method == 'POST':
-        form = MovieForm(instance=movie, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Успешно изменен <a href="/movies/"> На главную </a> ')
-    else:
-        form = MovieForm(instance=movie)
-    context = {
-        'form': form,
-        'movie': movie
-    }
-    return render(request, 'movies/movie_update.html', context)
+# def movie_detail_view(request, id):
+#     movie = get_object_or_404(Movie, id=id)
+#     return render(request, 'movies/movie_detail.html', {'movie': movie})
 
-def delete_movie_view(request, id):
-    movie = get_object_or_404(Movie, id=id)
-    movie.delete()
-    return HttpResponse('Фильм успешно удален')
+
+class CreateMovieView(CreateView):
+    queryset = Movie.objects.all()
+    form_class = MovieForm
+    template_name = 'movies/movie_create.html'
+    success_url = '/movies/'
+
+    def form_valid(self, form):
+        return super(CreateMovieView, self).form_valid(form=form)
+
+
+# def create_movie_view(request):
+#     if request.method == 'POST':
+#         form = MovieForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Успешно добавлен <a href="/"> На главную </a> ')
+#     else:
+#         form = MovieForm()
+#     return render(request, 'movies/movie_create.html', {'form': form})
+
+
+class UpdateMovieView(UpdateView):
+    template_name = 'movies/movie_update.html'
+    form_class = MovieForm
+    success_url = '/movies/'
+
+    def get_object(self, **kwargs):
+        movie_id = self.kwargs.get('id')
+        return get_object_or_404(Movie, id=movie_id)
+
+    def form_valid(self, form):
+        return super(UpdateMovieView, self).form_valid(form=form)
+
+
+# def update_movie_view(request, id):
+#     movie = get_object_or_404(Movie, id=id)
+#     if request.method == 'POST':
+#         form = MovieForm(instance=movie, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Успешно изменен <a href="/movies/"> На главную </a> ')
+#     else:
+#         form = MovieForm(instance=movie)
+#     context = {
+#         'form': form,
+#         'movie': movie
+#     }
+#     return render(request, 'movies/movie_update.html', context)
+
+
+class DeleteMovieView(DeleteView):
+    template_name = 'movies/movie_delete.html'
+    success_url = '/movies/'
+
+    def get_object(self, **kwargs):
+        movie_id = self.kwargs.get('id')
+        return get_object_or_404(Movie, id=movie_id)
+
+# def delete_movie_view(request, id):
+#     movie = get_object_or_404(Movie, id=id)
+#     movie.delete()
+#     return HttpResponse('Фильм успешно удален')
+
+
+class Search(ListView):
+    template_name = 'movies/movie_list.html'
+    context_object_name = 'movie'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return Movie.objects.filter(title__icontains=self.request.GET.get('q'))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
